71b14781ba282e8117a64a4b57e24218ff7c6eaf
Gopikrishna Reddy <Gopikrishna.Reddy@akqa.com>
2021-06-25 15:27:41 +0530

    Add site extenstion to retrieve home item.

---
+++ Sitecore.DemoProject.MVC.Web/Extensions/SiteExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Sites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.DemoProject.MVC.Web.Extensions
+{
+    public static class SiteExtensions
+    {
+        public static Item HomeItem(this SiteContext siteContext)
+        {
+            return Context.Database.GetItem(siteContext.StartPath);
+        }
+    }
+}
