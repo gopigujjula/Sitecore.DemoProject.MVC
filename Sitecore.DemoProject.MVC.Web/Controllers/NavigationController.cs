f1e964f091a2f7c1fd307066bc2dfe45e1c68852
Gopikrishna Reddy <Gopikrishna.Reddy@akqa.com>
2021-06-25 22:34:31 +0530

    build header navigation by crawling the tree.

---
+++ Sitecore.DemoProject.MVC.Web/Controllers/NavigationController.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.DemoProject.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.DemoProject.MVC.Web.Extensions;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Data.Fields;
+
+namespace Sitecore.DemoProject.MVC.Web.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            var model = new NavigationViewModel();
+            List<Navigation> navigations = new List<Navigation>();
+
+            var homeItem = Sitecore.Context.Site.HomeItem();
+            navigations.Add(BuildNavigation(homeItem));
+
+            if(homeItem.HasChildren)
+            {
+                foreach(Item childItem in homeItem.Children)
+                {
+                    CheckboxField hideInNavigation = childItem.Fields["Hide_in_Navigation"];
+                    if (hideInNavigation!=null && !hideInNavigation.Checked)
+                    {
+                        navigations.Add(BuildNavigation(childItem));
+                    }
+                }
+            }
+            model.Navigations = navigations;
+
+            return View(model);
+        }
+
+        private Navigation BuildNavigation(Item item)
+        {
+            return new Navigation
+            {
+                NavigationTitle = item.Fields["Navigation_Title"]?.Value,
+                NavigationLink = item.Url(),
+                ActiveClass = PageContext.Current.Item.ID == item.ID ? "active" : string.Empty
+            };
+        }
+    }
+}
---
+++ Sitecore.DemoProject.MVC.Web/Models/NavigationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.DemoProject.MVC.Web.Models
+{
+    public class NavigationViewModel
+    {
+        public List<Navigation> Navigations { get; set; }
+    }
+    public class Navigation
+    {
+        public string NavigationTitle { get; set; }
+        public string NavigationLink { get; set; }
+        public string ActiveClass { get; set; }
+    }
+}
