873b3eb2a292706db9951888206ff54ef26c9982
Gopikrishna Reddy <Gopikrishna.Reddy@akqa.com>
2021-07-31 14:02:00 +0530

    Custom link provider

---
+++ Sitecore.DemoProject.MVC.Web/CustomCode/CustomLinkProvider.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Buckets.Managers;
+using Sitecore.Buckets.Extensions;
+using Sitecore.Links.UrlBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.IO;
+
+namespace Sitecore.DemoProject.MVC.Web.CustomCode
+{
+    public class CustomLinkProvider : ItemUrlBuilder
+    {
+        public CustomLinkProvider(DefaultItemUrlBuilderOptions option) : base(option)
+        {
+
+        }
+        public override string Build(Sitecore.Data.Items.Item item, ItemUrlBuilderOptions options)
+        {
+            ///Books/2019/05/11/05/05/The-Last-Lecture
+            ///Books/The-Last-Lecture
+            if (BucketManager.IsItemContainedWithinBucket(item))
+            {
+                var bucketItem = item.GetParentBucketItemOrParent();
+                if (bucketItem != null && bucketItem.IsABucket())//books item
+                {
+                    var bucketUrl = base.Build(bucketItem, options);
+                    return FileUtil.MakePath(bucketUrl, item.Name.Replace(' ', '-'));
+                }
+            }
+            return base.Build(item, options);
+        }
+
+    }
+}
